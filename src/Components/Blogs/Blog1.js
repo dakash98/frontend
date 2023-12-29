c6086ab4364d4e3c0fb34a027c9eadd5be21b5dd
MukulKolte <mukulkolte@gmail.com>
2023-12-29 21:59:45 +0530

    Update: navbar in blogs pages.

--- src/Components/Blogs/Blog1.js
+++ src/Components/Blogs/Blog1.js
@@ -1,8 +1,10 @@
-import React,{useEffect} from 'react';
+import React, { useEffect } from 'react';
 import Navbar from '../Navbar';
 import Footer from '../Footer';
 import myImage1 from './image1.jpg';
 import Joiningoptions from '../Joiningoptions';
+import { BreadcrumbProfile, Breadcrumbhome } from '../Breadcrumbs';
+import Profilepage from '../Profilepage';
 
 const Blog1 = () => {
   useEffect(() => {
@@ -10,25 +12,40 @@ const Blog1 = () => {
   }, []);
   return (
     <div>
-      <Navbar />
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-        <div className="p-4 md:p-8 lg:mb-10 flex items-center justify-center min-h-screen">
-          <div className="rounded-lg p-4 md:p-8 lg:px-24">
-            <img
-              src={myImage1}
-              alt="Description of the image"
-              className="w-full h-auto max-w-full rounded-lg"
-            />
-            <div className="mt-4"> {/* Add margin to separate image and text */}
-              <h2 className="text-3xl font-bold mb-4 text-white">10th Board SSC Exam 2024</h2>
-              <p className="text-lg mb-4 text-white">
-                Maharashtra board exam is scheduled on March 1, 2024, and will be over by March 25, 2024. When there are only three months left for the examination, students should be focusing more on practicing the syllabus now which includes a revision and practicing the previous year question papers as much as possible. Time management can reduce stress easily and help you achieve the goal as well.
-              </p>
-              {/* Add more content as needed */}
-            </div>
+      <div className="sticky top-0 flex z-10">
+        <Navbar />
+      </div>
+
+      <div className="relative z-0">
+        <div>
+          <div id="home">
+            <Breadcrumbhome />
           </div>
+          <div id="profile" className="hidden">
+            <BreadcrumbProfile />
+          </div>
+
+          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+            <div className="p-4 md:p-8 lg:mb-10 flex items-center justify-center min-h-screen">
+              <div className="rounded-lg p-4 md:p-8 lg:px-24">
+                <img
+                  src={myImage1}
+                  alt="Description of the image"
+                  className="w-full h-auto max-w-full rounded-lg"
+                />
+                <div className="mt-4"> {/* Add margin to separate image and text */}
+                  <h2 className="text-3xl font-bold mb-4 text-white">10th Board SSC Exam 2024</h2>
+                  <p className="text-lg mb-4 text-white">
+                    Maharashtra board exam is scheduled on March 1, 2024, and will be over by March 25, 2024. When there are only three months left for the examination, students should be focusing more on practicing the syllabus now which includes a revision and practicing the previous year question papers as much as possible. Time management can reduce stress easily and help you achieve the goal as well.
+                  </p>
+                  {/* Add more content as needed */}
+                </div>
+              </div>
+            </div>
+            <Joiningoptions />
+          </div>
+          <Profilepage />
         </div>
-        <Joiningoptions />
       </div>
       <Footer />
     </div>
