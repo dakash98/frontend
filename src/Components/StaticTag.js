d7aed6ea904c481ec9fd2be30612addc2034f58e
jayesh9302 <jayeshchaudhari090302@gmail.com>
2024-01-21 00:03:41 +0530

    :Remove border for static tag

--- src/Components/StaticTag.js
+++ src/Components/StaticTag.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function StaticTag() {
   return (
-    <div className="text-white border-2 border-solid border-blue-400  p-2 ml-[10%] md:ml-[46%] lg:ml-[55%] xl:ml-[72%]">
+    <div className="text-white p-2 ml-[10%] md:ml-[46%] lg:ml-[55%] xl:ml-[72%]">
       <h1 className="text-sm sm:text-sm md:text-lg lg:text-lg xl:text-lg ">10th SSC Maharashtra Board Question Paper</h1>
     </div>
   );
