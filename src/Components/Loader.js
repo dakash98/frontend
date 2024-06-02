d4f2c36a49e05e72fb9adecccc5735180e097a69
PraveennKumar <praveensinghh0528@gmail.com>
2024-06-02 22:17:13 +0530

    loader

--- src/Components/Loader.js
+++ src/Components/Loader.js
@@ -1,12 +1,12 @@
 import React from 'react'
 // import loader from '../Static/website-loader.gif'
-import HashLoader from 'react-spinners/HashLoader';
+import RingLoader from 'react-spinners/RingLoader';
 // HashLoader
 
 function Loader() {
   return (
     <div className='bg-white bg-opacity-0 w-full h-screen flex justify-center items-center'>
-    <HashLoader color={"#DAFF33 "} size={120} />
+    <RingLoader color={"#DAFF33 "} size={120} />
   </div>
   )
 }
